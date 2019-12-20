f7e3df65fb2351f2496bc5a6dff3a6e5a0d0cecc
Niklas <niklas.toetsch@stud.uni-due.de>
2019-12-20 12:47:15 +0100

    test if mean and variance of dirichlet and DCM are properly sampled

--- test/test_cmanalysis.py
+++ test/test_cmanalysis.py
@@ -11,11 +11,38 @@ class TestConfusionMatrixAnalyser(ut.TestCase):
         input_cm = pd.Series([9, 1, 3, 2], index=['TP', 'FN', 'TN', 'FP'])
         # use improper prior to avoid bias / simplifies calculation
         self.analyser = ConfusionMatrixAnalyser(input_cm, prior=haldane_prior)
+        self.N = self.analyser.confusion_matrix.values.sum()
 
         sel_n = 100000
         inf_n_pp = self.analyser.posterior_predict_confusion_matrices(N=sel_n)
         inf_n_pp /= float(sel_n)
         self.inf_n_pp = inf_n_pp
+
+    def test_theta_and_x_sampling(self):
+        """confirm that sampled expected value/variance for theta and X are close to the analytical solution
+        see https://en.wikipedia.org/wiki/Dirichlet_distribution
+        and https://en.wikipedia.org/wiki/Dirichlet-multinomial_distribution"""
+
+        alpha = self.analyser.prior + self.analyser.confusion_matrix
+        alpha_0 = float(sum(alpha))
+        dirichlet_mean = alpha / alpha_0
+        dcm_mean = self.N * dirichlet_mean
+        dirichlet_var = dirichlet_mean * (1 - dirichlet_mean) / (alpha_0 + 1)
+        dcm_var = self.N * (self.N + alpha_0) * dirichlet_var
+
+        for i in self.analyser.theta_samples:
+            self.assertAlmostEqual(dirichlet_mean[i],
+                                   self.analyser.theta_samples[i].mean(),
+                                   delta=1e-2)
+            self.assertAlmostEqual(dcm_mean[i],
+                                   self.analyser.pp_samples[i].mean(),
+                                   delta=5e-2)
+            self.assertAlmostEqual(dirichlet_var[i],
+                                   self.analyser.theta_samples[i].var(),
+                                   delta=1e-3)
+            self.assertAlmostEqual(dcm_var[i],
+                                   self.analyser.pp_samples[i].var(),
+                                   delta=2e-1)
 
     def test_expected_value(self):
         """confirm that expected value is equal to the metric for the original confusion matrix
